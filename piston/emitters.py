5ce7be20ee568f2aa182fe0501bdde9e8de1e187
saw2th <saw2th@localhost>
2015-09-11 12:59:46 +0100

    django 1.8 changes

--- piston/emitters.py
+++ piston/emitters.py
@@ -21,7 +21,7 @@ except NameError:
 
 from django.db.models.query import QuerySet
 from django.db.models import Model, permalink
-from django.utils import simplejson
+import json as simplejson
 from django.utils.xmlutils import SimplerXMLGenerator
 from django.utils.encoding import smart_unicode
 from django.core.serializers.json import DateTimeAwareJSONEncoder
